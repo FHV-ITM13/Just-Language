721444c8dd2e9d2b0d351419f21b804c2475f44f
Michael Zangerle <michael.zangerle@gmail.com>
2014-05-04 19:03:24 +0200

    worked on codegenerator

--- Michael_JustCompiler/src/fhv/codegenerator/CodeGenerator.java
+++ Michael_JustCompiler/src/fhv/codegenerator/CodeGenerator.java
@@ -3,20 +3,34 @@ package fhv.codegenerator;
 import fhv.xml.writer.XMLClassWriter;
 
 public class CodeGenerator {
-	
+
+	public enum ConstantType {
+		CLAZZ, DATATYPE, UTF8
+	};
+
 	private XMLClassWriter xmlWriter;
-	public static CodeGenerator codeGenerator = new CodeGenerator();
-	
-	private CodeGenerator(){
-		this.xmlWriter = new XMLClassWriter("class.xml");
+
+	public CodeGenerator(String classFile) {
+		this.xmlWriter = new XMLClassWriter(classFile + ".xml", "justclassfile");
 	}
-	
-	public void buildFile(){
+
+	public void buildFile() {
 		this.xmlWriter.endClassFile();
 	}
-	
-	public void addConstantClass(int index, int nameIndex){
-		this.xmlWriter.addConstantClass(String.valueOf(index),String.valueOf(nameIndex));
+
+	public void addConstant(int index, String value, ConstantType type) {
+		switch (type) {
+		case CLAZZ:
+			this.xmlWriter.addConstantClass(String.valueOf(index), value);
+			break;
+		case DATATYPE:
+			this.xmlWriter.addConstantDatatype(String.valueOf(index), value);
+			break;
+		default:
+			this.xmlWriter.addConstantUTF8(String.valueOf(index), value);
+			break;
+		}
+
 	}
 
 }
--- Michael_JustCompiler/src/fhv/xml/writer/XMLClassWriter.java
+++ Michael_JustCompiler/src/fhv/xml/writer/XMLClassWriter.java
@@ -18,7 +18,7 @@ public class XMLClassWriter {
 
 	private Document document;
 	private String filename;
-	
+
 	private Element classElement;
 	private Element magicElement;
 	private Element minorVersionElement;
@@ -28,53 +28,71 @@ public class XMLClassWriter {
 	private Element fieldInfoElement;
 	private Element methodInfoElement;
 
-	public XMLClassWriter(String filename) {
+	/**
+	 * Inits the basic nodes for the xml class file
+	 * @param filename
+	 * @param classFile
+	 */
+	public XMLClassWriter(String filename, String classFile) {
 		try {
-			DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
+			DocumentBuilderFactory docFactory = DocumentBuilderFactory
+					.newInstance();
 			DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
-			
+
 			this.document = docBuilder.newDocument();
 			this.filename = filename;
-			
-			this.classElement = this.document.createElement("class");
+
+			this.classElement = this.document.createElement("classfile");
 			this.document.appendChild(this.classElement);
-			
+
 			this.magicElement = this.document.createElement("magic");
-			this.magicElement.appendChild(this.document.createTextNode("magicNumber"));
+			this.magicElement.appendChild(this.document
+					.createTextNode(classFile));
 			this.classElement.appendChild(this.magicElement);
-			
-			this.minorVersionElement = this.document.createElement("minor_version");
-			this.minorVersionElement.appendChild(this.document.createTextNode("0"));
+
+			this.minorVersionElement = this.document
+					.createElement("minor_version");
+			this.minorVersionElement.appendChild(this.document
+					.createTextNode("0"));
 			this.classElement.appendChild(this.minorVersionElement);
-			
-			this.majorVersionElement = this.document.createElement("majorversion");
-			this.majorVersionElement.appendChild(this.document.createTextNode("1"));
+
+			this.majorVersionElement = this.document
+					.createElement("majorversion");
+			this.majorVersionElement.appendChild(this.document
+					.createTextNode("1"));
 			this.classElement.appendChild(this.majorVersionElement);
-			
+
 			this.thisClassElement = this.document.createElement("this_class");
-			this.thisClassElement.appendChild(this.document.createTextNode("1"));
+			this.thisClassElement
+					.appendChild(this.document.createTextNode("1"));
 			this.classElement.appendChild(this.thisClassElement);
-			
-			this.constantPoolElement = this.document.createElement("constant_pool");
+
+			this.constantPoolElement = this.document
+					.createElement("constant_pool");
 			this.classElement.appendChild(this.constantPoolElement);
-			
+
 			this.fieldInfoElement = this.document.createElement("field_info");
 			this.classElement.appendChild(this.fieldInfoElement);
-			
+
 			this.methodInfoElement = this.document.createElement("method_info");
 			this.classElement.appendChild(this.methodInfoElement);
-			
+
 		} catch (ParserConfigurationException e) {
-			System.out.println("Error during xml-builder-configuration: "+ e.getMessage());
+			System.out.println("Error during xml-builder-configuration: "
+					+ e.getMessage());
 		}
 	}
 
+	/**
+	 * Finishes the XML class file and actually writs the file to disk
+	 */
 	public void endClassFile() {
 
 		try {
-			TransformerFactory transformerFactory = TransformerFactory.newInstance();
+			TransformerFactory transformerFactory = TransformerFactory
+					.newInstance();
 			Transformer transformer = transformerFactory.newTransformer();
-			
+
 			DOMSource source = new DOMSource(this.document);
 			StreamResult result = new StreamResult(new File(this.filename));
 
@@ -86,17 +104,91 @@ public class XMLClassWriter {
 		}
 	}
 
+	/**
+	 * Adds a constant node for the class (program)
+	 * @param index
+	 * @param nameIndex
+	 */
+	public void addConstantClass(String index, String nameIndex) {	
+		this.addConstantNode("constant_class", index, "name_index", nameIndex);
+	}
+
+	/**
+	 * Adds a constant node for any identifier
+	 * @param index
+	 * @param value
+	 */
+	public void addConstantUTF8(String index, String value) {
+		this.addConstantNode("constant_utf8", index, "bytes", value);
+	}
+
+	/**
+	 * Adds a constant node for a datatype
+	 * @param index
+	 * @param value
+	 */
+	public void addConstantDatatype(String index, String value) {
+		String nodeValue = this.getJVMType(value);
+		this.addConstantNode("constant_utf8", index, "bytes", nodeValue);
+	}
 	
-	public void addConstantClass(String index, String nameIndex) {
-		Element constantClass = document.createElement("constant_class");
-		constantClass.setAttribute("index", index);
+	/**
+	 * Creates a constant node
+	 * @param nodeName
+	 * @param index
+	 * @param childeNodeName
+	 * @param value
+	 */
+	private void addConstantNode(String nodeName, String index, String childeNodeName, String value) {
+		Element constantElement = document.createElement(nodeName);
+		constantElement.setAttribute("index", index);
+
+		Element childElement = document.createElement(childeNodeName);
+		childElement.appendChild(this.document.createTextNode(value));
+		constantElement.appendChild(childElement);
 		
-		Element nameIndexEl = document.createElement("name_index");
-		nameIndexEl.appendChild(this.document.createTextNode(nameIndex));
-		constantClass.appendChild(nameIndexEl);
-		
-		this.constantPoolElement.appendChild(constantClass);
-		
+		this.constantPoolElement.appendChild(constantElement);
+	}
+
+	/**
+	 * Returns for a datatype the JVM datatype
+	 * @param value
+	 * @return
+	 */
+	private String getJVMType(String value) {
+
+		// TODO add array support
+		// http://stackoverflow.com/questions/9909228/what-does-v-mean-in-a-class-signature
+
+		switch (value) {
+		case "byte":
+			return "B";
+
+		case "char":
+			return "C";
+
+		case "double":
+			return "d";
+
+		case "float":
+			return "F";
+
+		case "long":
+			return "J";
+
+		case "short":
+			return "S";
+
+		case "boolean":
+			return "Z";
+
+		case "void":
+			return "V";
+
+		default:
+			return "I";
+
+		}
 	}
 
 }

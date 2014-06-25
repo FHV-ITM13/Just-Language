ec87654079f5d8b10fd0324c33968763a6b95838
Johannes Schwendinger <johannes.schwendinger@hotmail.com>
2014-06-25 15:17:45 +0200

    Added my stuff

---
+++ JohannesSAufgabe3/src/fhv/q3/Johannesmain.java
@@ -0,0 +1,62 @@
+package fhv.q3;
+
+public class Johannesmain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Johannesmain m = new Johannesmain();
+		char[] tape = new char[] { 'z','x','z'};
+		if (m.accept(tape)) {
+			System.out.println("Accept");
+		} else {
+			System.out.println("Nicht Accept");
+		}
+
+	}
+
+	public Boolean accept(char[] tape) {
+		for (char c : tape) {
+			c = new String(new char[] { c }).toLowerCase().toCharArray()[0];
+		}
+		char z = 'z';
+		char x = 'x';
+		char y = 'y';
+		State cur = State.A;
+
+		for (char c : tape) {
+			switch (cur) {
+			case A:
+				if (c == z)
+					cur = State.A;
+				else if (c == x)
+					cur = State.B;
+				else
+					return false;
+				break;
+			case B:
+				if (c == z)
+					cur = State.C;
+				else if (c == y)
+					cur = State.D;
+				else
+					return false;
+				break;
+			case C:
+				return false;
+			case D:
+				if (c == z)
+					cur = State.C;
+				else
+					return false;
+				break;
+			default:
+				return false;
+			}
+		}
+		return cur == State.C;
+	}
+
+	enum State {
+		A, B, C, D
+	}
+}

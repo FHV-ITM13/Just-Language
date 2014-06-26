1738bdb956fd74b08143f55dd5929497d1b718e9
Johannes Schwendinger <johannes.schwendinger@hotmail.com>
2014-06-26 15:20:17 +0200

    accept2 added

--- JohannesSAufgabe3/src/fhv/q3/Johannesmain.java
+++ JohannesSAufgabe3/src/fhv/q3/Johannesmain.java
@@ -5,8 +5,8 @@ public class Johannesmain {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Johannesmain m = new Johannesmain();
-		char[] tape = new char[] { 'z', 'X', 'z' };
-		if (m.accept(tape)) {
+		
+		if (m.accept2("zzzzfzxz".toCharArray())) {
 			System.out.println("Accept");
 		} else {
 			System.out.println("Nicht Accept");
@@ -14,6 +14,26 @@ public class Johannesmain {
 
 	}
 
+	public boolean accept2(char[] tape){
+		char[][] states = new char[128][128];
+		
+		states['A']['z'] = 'A';
+		states['A']['x'] = 'B';
+		states['B']['y'] = 'D';
+		states['B']['z'] = 'C';
+		states['D']['z'] = 'C';
+		
+		char cur = 'A';
+		
+		for(char c:tape){
+			cur = states[cur][c];
+			if(cur == 0)
+				return false;
+		}
+		return cur =='C';
+	}
+	
+	
 	public Boolean accept(char[] tape) {
 		tape = new String(tape).toLowerCase().toCharArray();
 		char z = 'z';
